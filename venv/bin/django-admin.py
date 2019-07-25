1a0f0b28d84440fc03c3d07b720d05bd9409aeb8
Maciej <ext-maciej.piankowski@drutex.com.pl>
2019-07-25 08:31:00 +0200

    My App, first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,7 @@
+#!/home/maciej/PycharmProjects/dj/venv/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.0.dev20190724120837','django-admin.py'
+__requires__ = 'Django==3.0.dev20190724120837'
+__import__('pkg_resources').require('Django==3.0.dev20190724120837')
+__file__ = '/home/maciej/PycharmProjects/dj/django/django/bin/django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
